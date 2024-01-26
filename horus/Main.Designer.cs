0f16fb8b3c1eb6b5dbb4a9b6a14d89d4a0489a3f
Alexandre Delaval-Dupuis <alexandre.delaval.dupuis@gmail.com>
2024-01-26 15:14:43 +0100

    ajout des liens entre les boutons du main et les forms

--- horus/Main.Designer.cs
+++ horus/Main.Designer.cs
@@ -28,59 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             btnPersonneEntree = new Button();
             btnPersonneSortie = new Button();
             btnEvenementAjout = new Button();
             btnEvenementSuppression = new Button();
+            picLogo = new PictureBox();
+            picParametres = new PictureBox();
+            picTelechargement = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picParametres).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picTelechargement).BeginInit();
             SuspendLayout();
             // 
             // btnPersonneEntree
             // 
-            btnPersonneEntree.Location = new Point(174, 119);
+            btnPersonneEntree.BackColor = SystemColors.Control;
+            btnPersonneEntree.Location = new Point(153, 147);
             btnPersonneEntree.Name = "btnPersonneEntree";
             btnPersonneEntree.Size = new Size(150, 86);
             btnPersonneEntree.TabIndex = 0;
             btnPersonneEntree.Text = "Une personne est entrée";
-            btnPersonneEntree.UseVisualStyleBackColor = true;
+            btnPersonneEntree.UseVisualStyleBackColor = false;
+            btnPersonneEntree.Click += btnPersonneEntree_Click;
             // 
             // btnPersonneSortie
             // 
-            btnPersonneSortie.Location = new Point(496, 119);
+            btnPersonneSortie.BackColor = SystemColors.Control;
+            btnPersonneSortie.Location = new Point(475, 147);
             btnPersonneSortie.Name = "btnPersonneSortie";
             btnPersonneSortie.Size = new Size(150, 86);
             btnPersonneSortie.TabIndex = 1;
             btnPersonneSortie.Text = "Une personne est sortie";
-            btnPersonneSortie.UseVisualStyleBackColor = true;
+            btnPersonneSortie.UseVisualStyleBackColor = false;
+            btnPersonneSortie.Click += btnPersonneSortie_Click;
             // 
             // btnEvenementAjout
             // 
-            btnEvenementAjout.Location = new Point(174, 269);
+            btnEvenementAjout.BackColor = SystemColors.Control;
+            btnEvenementAjout.Location = new Point(153, 297);
             btnEvenementAjout.Name = "btnEvenementAjout";
             btnEvenementAjout.Size = new Size(150, 86);
             btnEvenementAjout.TabIndex = 2;
             btnEvenementAjout.Text = "Ajouter un évènement";
-            btnEvenementAjout.UseVisualStyleBackColor = true;
+            btnEvenementAjout.UseVisualStyleBackColor = false;
+            btnEvenementAjout.Click += btnEvenementAjout_Click;
             // 
             // btnEvenementSuppression
             // 
-            btnEvenementSuppression.Location = new Point(496, 269);
+            btnEvenementSuppression.BackColor = SystemColors.Control;
+            btnEvenementSuppression.Location = new Point(475, 297);
             btnEvenementSuppression.Name = "btnEvenementSuppression";
             btnEvenementSuppression.Size = new Size(150, 86);
             btnEvenementSuppression.TabIndex = 3;
             btnEvenementSuppression.Text = "Supprimer un évènement";
-            btnEvenementSuppression.UseVisualStyleBackColor = true;
+            btnEvenementSuppression.UseVisualStyleBackColor = false;
+            btnEvenementSuppression.Click += btnEvenementSuppression_Click;
+            // 
+            // picLogo
+            // 
+            picLogo.Image = (Image)resources.GetObject("picLogo.Image");
+            picLogo.Location = new Point(30, 12);
+            picLogo.Name = "picLogo";
+            picLogo.Size = new Size(153, 118);
+            picLogo.TabIndex = 4;
+            picLogo.TabStop = false;
+            // 
+            // picParametres
+            // 
+            picParametres.BackColor = SystemColors.Control;
+            picParametres.Image = (Image)resources.GetObject("picParametres.Image");
+            picParametres.Location = new Point(706, 29);
+            picParametres.Name = "picParametres";
+            picParametres.Size = new Size(50, 51);
+            picParametres.TabIndex = 5;
+            picParametres.TabStop = false;
+            picParametres.Click += picParametres_Click;
+            // 
+            // picTelechargement
+            // 
+            picTelechargement.BackColor = SystemColors.Control;
+            picTelechargement.Image = (Image)resources.GetObject("picTelechargement.Image");
+            picTelechargement.Location = new Point(612, 29);
+            picTelechargement.Name = "picTelechargement";
+            picTelechargement.Size = new Size(50, 51);
+            picTelechargement.TabIndex = 6;
+            picTelechargement.TabStop = false;
+            picTelechargement.Click += picTelechargement_Click;
             // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(picTelechargement);
+            Controls.Add(picParametres);
+            Controls.Add(picLogo);
             Controls.Add(btnEvenementSuppression);
             Controls.Add(btnEvenementAjout);
             Controls.Add(btnPersonneSortie);
             Controls.Add(btnPersonneEntree);
             Name = "Main";
             Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picParametres).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picTelechargement).EndInit();
             ResumeLayout(false);
         }
 
@@ -90,5 +142,8 @@
         private Button btnPersonneSortie;
         private Button btnEvenementAjout;
         private Button btnEvenementSuppression;
+        private PictureBox picLogo;
+        private PictureBox picParametres;
+        private PictureBox picTelechargement;
     }
 }
--- horus/main.cs
+++ horus/main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using horus.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,42 @@ namespace horus
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void btnPersonneEntree_Click(object sender, EventArgs e)
+        {
+            PersonneEntreeSortie personneEntreeSortie = new PersonneEntreeSortie();
+            personneEntreeSortie.Show();
+        }
+
+        private void btnPersonneSortie_Click(object sender, EventArgs e)
+        {
+            PersonneEntreeSortie personneEntreeSortie = new PersonneEntreeSortie();
+            personneEntreeSortie.Show();
+        }
+
+        private void btnEvenementAjout_Click(object sender, EventArgs e)
+        {
+            EvenementEntreeSortie evenementEntreeSortie = new EvenementEntreeSortie();
+            evenementEntreeSortie.Show();
+        }
+
+        private void btnEvenementSuppression_Click(object sender, EventArgs e)
+        {
+            EvenementEntreeSortie evenementEntreeSortie = new EvenementEntreeSortie();
+            evenementEntreeSortie.Show();
+        }
+
+        private void picTelechargement_Click(object sender, EventArgs e)
+        {
+            Telechargement telechargement = new Telechargement();
+            telechargement.Show();
+        }
+
+        private void picParametres_Click(object sender, EventArgs e)
+        {
+            Parametres parametres = new Parametres();
+            parametres.Show();
         }
     }
 }
