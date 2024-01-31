b69f386e36fcf4bb6f45fa2092ab041802494062
Alexandre Delaval-Dupuis <alexandre.delaval.dupuis@gmail.com>
2024-01-31 10:42:28 +0100

    - changement de la résolution du main, et donc modification de la taille et de la position de chaque élément sur le form

--- horus/MainForm.Designer.cs
+++ horus/MainForm.Designer.cs
@@ -46,63 +46,59 @@
             // btnPersonneEntree
             // 
             btnPersonneEntree.BackColor = Color.PaleTurquoise;
-            btnPersonneEntree.Font = new Font("Sans Serif Collection", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPersonneEntree.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnPersonneEntree.ForeColor = SystemColors.ControlText;
-            btnPersonneEntree.Location = new Point(219, 245);
-            btnPersonneEntree.Margin = new Padding(4, 5, 4, 5);
+            btnPersonneEntree.Location = new Point(411, 233);
             btnPersonneEntree.Name = "btnPersonneEntree";
-            btnPersonneEntree.Size = new Size(214, 176);
+            btnPersonneEntree.Size = new Size(275, 224);
             btnPersonneEntree.TabIndex = 1;
-            btnPersonneEntree.Text = "Personne ENTREE";
+            btnPersonneEntree.Text = "Personne entrée";
             btnPersonneEntree.UseVisualStyleBackColor = false;
             btnPersonneEntree.Click += btnPersonneEntree_Click;
             // 
             // btnPersonneSortie
             // 
             btnPersonneSortie.BackColor = Color.PaleTurquoise;
-            btnPersonneSortie.Font = new Font("Sans Serif Collection", 9F);
-            btnPersonneSortie.Location = new Point(679, 245);
-            btnPersonneSortie.Margin = new Padding(4, 5, 4, 5);
+            btnPersonneSortie.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPersonneSortie.Location = new Point(1229, 226);
             btnPersonneSortie.Name = "btnPersonneSortie";
-            btnPersonneSortie.Size = new Size(214, 168);
+            btnPersonneSortie.Size = new Size(275, 224);
             btnPersonneSortie.TabIndex = 2;
-            btnPersonneSortie.Text = "Personne SORTIE";
+            btnPersonneSortie.Text = "Personne sortie";
             btnPersonneSortie.UseVisualStyleBackColor = false;
             btnPersonneSortie.Click += btnPersonneSortie_Click;
             // 
             // btnEvenementAjout
             // 
             btnEvenementAjout.BackColor = Color.PaleTurquoise;
-            btnEvenementAjout.Font = new Font("Sans Serif Collection", 9F);
-            btnEvenementAjout.Location = new Point(219, 495);
-            btnEvenementAjout.Margin = new Padding(4, 5, 4, 5);
+            btnEvenementAjout.Font = new Font("Segoe UI", 20.25F);
+            btnEvenementAjout.Location = new Point(411, 622);
             btnEvenementAjout.Name = "btnEvenementAjout";
-            btnEvenementAjout.Size = new Size(214, 156);
+            btnEvenementAjout.Size = new Size(275, 224);
             btnEvenementAjout.TabIndex = 3;
-            btnEvenementAjout.Text = "DEBUT  évènement";
+            btnEvenementAjout.Text = "Début  évènement";
             btnEvenementAjout.UseVisualStyleBackColor = false;
             btnEvenementAjout.Click += btnEvenementAjout_Click;
             // 
             // btnEvenementSuppression
             // 
             btnEvenementSuppression.BackColor = Color.PaleTurquoise;
-            btnEvenementSuppression.Font = new Font("Sans Serif Collection", 9F);
-            btnEvenementSuppression.Location = new Point(679, 495);
-            btnEvenementSuppression.Margin = new Padding(4, 5, 4, 5);
+            btnEvenementSuppression.Font = new Font("Segoe UI", 20.25F);
+            btnEvenementSuppression.Location = new Point(1229, 622);
             btnEvenementSuppression.Name = "btnEvenementSuppression";
-            btnEvenementSuppression.Size = new Size(214, 156);
+            btnEvenementSuppression.Size = new Size(275, 224);
             btnEvenementSuppression.TabIndex = 4;
-            btnEvenementSuppression.Text = " FIN évènement";
+            btnEvenementSuppression.Text = " Fin évènement";
             btnEvenementSuppression.UseVisualStyleBackColor = false;
             btnEvenementSuppression.Click += btnEvenementSuppression_Click;
             // 
             // picLogo
             // 
             picLogo.Image = (Image)resources.GetObject("picLogo.Image");
-            picLogo.Location = new Point(42, 20);
-            picLogo.Margin = new Padding(4, 5, 4, 5);
+            picLogo.Location = new Point(12, 12);
             picLogo.Name = "picLogo";
-            picLogo.Size = new Size(219, 196);
+            picLogo.Size = new Size(300, 200);
+            picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
             picLogo.TabIndex = 4;
             picLogo.TabStop = false;
             // 
@@ -110,10 +106,9 @@
             // 
             picParametres.BackColor = SystemColors.Control;
             picParametres.Image = (Image)resources.GetObject("picParametres.Image");
-            picParametres.Location = new Point(1008, 49);
-            picParametres.Margin = new Padding(4, 5, 4, 5);
+            picParametres.Location = new Point(1747, 30);
             picParametres.Name = "picParametres";
-            picParametres.Size = new Size(80, 85);
+            picParametres.Size = new Size(100, 100);
             picParametres.SizeMode = PictureBoxSizeMode.StretchImage;
             picParametres.TabIndex = 5;
             picParametres.TabStop = false;
@@ -123,10 +118,9 @@
             // 
             picTelechargement.BackColor = SystemColors.Control;
             picTelechargement.Image = (Image)resources.GetObject("picTelechargement.Image");
-            picTelechargement.Location = new Point(910, 49);
-            picTelechargement.Margin = new Padding(4, 5, 4, 5);
+            picTelechargement.Location = new Point(1595, 29);
             picTelechargement.Name = "picTelechargement";
-            picTelechargement.Size = new Size(71, 85);
+            picTelechargement.Size = new Size(100, 100);
             picTelechargement.SizeMode = PictureBoxSizeMode.StretchImage;
             picTelechargement.TabIndex = 6;
             picTelechargement.TabStop = false;
@@ -135,29 +129,29 @@
             // lblDate
             // 
             lblDate.AutoSize = true;
-            lblDate.Location = new Point(504, 91);
-            lblDate.Margin = new Padding(4, 0, 4, 0);
+            lblDate.Font = new Font("Segoe UI", 20F);
+            lblDate.Location = new Point(700, 82);
             lblDate.Name = "lblDate";
-            lblDate.Size = new Size(49, 25);
+            lblDate.Size = new Size(73, 37);
             lblDate.TabIndex = 7;
             lblDate.Text = "Date";
             // 
             // lblHeure
             // 
             lblHeure.AutoSize = true;
-            lblHeure.Location = new Point(602, 91);
-            lblHeure.Margin = new Padding(4, 0, 4, 0);
+            lblHeure.Font = new Font("Segoe UI", 20F);
+            lblHeure.Location = new Point(1160, 82);
             lblHeure.Name = "lblHeure";
-            lblHeure.Size = new Size(59, 25);
+            lblHeure.Size = new Size(88, 37);
             lblHeure.TabIndex = 8;
             lblHeure.Text = "Heure";
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLightLight;
-            ClientSize = new Size(1142, 750);
+            ClientSize = new Size(1904, 1041);
             Controls.Add(lblHeure);
             Controls.Add(lblDate);
             Controls.Add(picTelechargement);
@@ -167,7 +161,6 @@
             Controls.Add(btnEvenementAjout);
             Controls.Add(btnPersonneSortie);
             Controls.Add(btnPersonneEntree);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "MainForm";
             Text = "Main";
             ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
